a5ef89fbc5e72989b643c4511c266b5a442c241d
Franklyn Zhu <zfranklyn@gmail.com>
2017-12-01 23:39:12 -0500

    set up wireframe for mobile ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Grid,
+  Row,
+  Col,
+  Image,
+  Tabs,
+  Tab,
+  Button,
+} from 'react-bootstrap';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemName: '',
+      itemDescription: '',
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      itemName: 'Bench Press',
+      itemDescription: `Transform any space into a personal free weight fitness center
+      Adjusts to six different positions, 17-degree decline to 90-degree incline
+      Comes with a removable leg hold-down brace for added decline position support
+      Corrects posture and stabilizes positioning for a safe, muscle-building free-weight workout
+      30-Year warranty on frame, one-year warranty on upholstery`,
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Image className="main-img" src="/bench.jpeg" responsive />
+        <Grid>
+          <Row>
+            <Col xs={12}>
+            <Tabs defaultActiveKey={1} id="nav-tab">
+              <Tab eventKey={1} title="Overview">
+                <h3>
+                  {this.state.itemName}
+                </h3>
+                <p>
+                  {this.state.itemDescription}
+                </p>
+              </Tab>
+              <Tab eventKey={2} title="Actions">
+                <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
+              </Tab>
+            </Tabs>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
