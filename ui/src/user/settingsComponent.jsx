cd91acaf252f4f6d73a3fed4c8dead2b88ea1846
Alfie Orumnila <ogunthunder@Alfies-MacBook-Pro.local>
2017-12-02 14:36:58 -0500

    More view implementations

--- ui/src/user/settingsComponent.jsx
+++ ui/src/user/settingsComponent.jsx
@@ -1,21 +1,90 @@
 import React, { Component } from 'react';
 import {
     Modal,
+    FormControl,
+    ControlLabel,
+    Button,
+    FormGroup,
+    Tabs,
+    Tab,
 } from 'react-bootstrap';
+import faker from 'faker';
 
 class SettingsComponent extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+        loaded: false,
+        userData: null,
+    }
+  }
+
+  componentDidMount() {
+    // fetch and update user data
+    this.setState({
+        loaded: true,
+        userData: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            scanHistory: [
+                {timestamp: Date(), 
+                scanTarget: 3,
+                location: `${faker.address.latitude()} ${faker.address.longitude()}`,
+                },
+                {timestamp: Date(), 
+                    scanTarget: 3,
+                    location: `${faker.address.latitude()} ${faker.address.longitude()}`,
+                },
+            ]
+        }
+    })
   }
 
   render() {
-      if (this.props.accountCreated) {
+      // this.props.accountCreated
+      if (true && this.state.loaded) {
           return (
             <Modal show={this.props.showModal} onHide={this.props.closeModal}>
             <Modal.Header closeButton>
                 <Modal.Title>Settings</Modal.Title>
             </Modal.Header>
+            <Modal.Body>
+            <Tabs defaultActiveKey={1}>
+                <Tab eventKey={1} title="Your Data">
+                    <FormGroup>
+                        <ControlLabel>First Name</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.userData.firstName}
+                            placeholder="Enter First Name"
+                        />
+                        <ControlLabel>Last Name</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.userData.lastName}
+                            placeholder="Enter Last Name"
+                        />
+                        <ControlLabel>Email</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.userData.email}
+                            placeholder="Enter Email"
+                        />                    
+                        </FormGroup>                    
+                </Tab>
+                <Tab eventKey={2} title="Scan History">
+                    Scan History
+                </Tab>
+                <Tab eventKey={3} title="Privacy">
+                    Privacy
+                </Tab>
+            </Tabs>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button bsStyle="primary" bsSize="large" block>Save Changes</Button>
+            </Modal.Footer>
 
         </Modal>
           );
@@ -25,8 +94,30 @@ class SettingsComponent extends Component {
         <div>
             <Modal show={this.props.showModal} onHide={this.props.closeModal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>{this.props.accountCreated ? 'Settings' : 'Sign Up'}</Modal.Title>
+                    <Modal.Title>Sign up</Modal.Title>
                 </Modal.Header>
+                <Modal.Body> 
+                <form>
+                    <FormGroup>
+                    <ControlLabel>First Name</ControlLabel>
+                    <FormControl
+                        type="text"
+                        placeholder="Enter First Name"
+                    />
+                    <ControlLabel>Last Name</ControlLabel>
+                    <FormControl
+                        type="text"
+                        placeholder="Enter Last Name"
+                    />
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        type="text"
+                        placeholder="Enter Email"
+                    />                    
+                    </FormGroup>
+                    <Button bsStyle="primary" bsSize="large" block>Create My Account</Button>
+                </form>                    
+                </Modal.Body>
 
             </Modal>
         </div>
