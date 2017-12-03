a11e46458143e71113e280247f64375add2ebdcd
Franklyn Zhu <zfranklyn@gmail.com>
2017-12-03 02:13:07 -0500

    new UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,22 +8,21 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
+  Switch,
 } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <div>
-            {/* <Route path="/" component={<div>Landing Page Placeholder</div>}/> */}
-            <Route path="/u/viewproduct/:productId" component={viewProduct}/>
-            {/* <Route path="/u/editProduct/:productId" component={<div>Edit Product</div>}/> */}
-            {/* <Route path="/u/settings" component={<div>Settings</div>}/> */}
-            <Route path="/u/addproduct" component={addProduct}/>
-            <Route path="/" component={VendorSection}/>
-          </div>
-        </div>
+        <Switch>
+          {/* <Route path="/" component={<div>Landing Page Placeholder</div>}/> */}
+          <Route path="/u/viewproduct/:productId" component={viewProduct}/>
+          {/* <Route path="/u/editProduct/:productId" component={<div>Edit Product</div>}/> */}
+          {/* <Route path="/u/settings" component={<div>Settings</div>}/> */}
+          <Route path="/u/addproduct" component={addProduct}/>
+          <Route path="/" component={VendorSection}/>
+        </Switch>
       </Router>
     )
   }  
